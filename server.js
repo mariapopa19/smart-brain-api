4fd3d764189d4781794aab2e51103e3df56226e2
Maria Popa <maria_olimp@yahoo.com>
2022-11-08 20:54:24 +0200

    adding port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.db.users;
+  res.send("it is working!");
 });
 
 app.post("/signin", signin.handleSignin(db, bcrypt));
